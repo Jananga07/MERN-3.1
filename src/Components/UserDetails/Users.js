e80df2160528184e32e3c1c93aa598fa2b28b2a5
Jananga07 <janangachandima07@gmail.com>
2026-02-08 22:43:36 +0530

    today

--- src/Components/UserDetails/Users.js
+++ src/Components/UserDetails/Users.js
@@ -1,12 +1,16 @@
 import React from 'react'
 import Nav from '../Nav/Nav';
+import axios from "axios";
 
+const URL ="http://localhost:5000/users";
+
+const fetchHandle
 
 function Users() {
   return (
     <div>
       <Nav/>
-      <h1>User Details</h1>
+      <h1>User Details Display Page</h1>
     </div>
   )
 }
